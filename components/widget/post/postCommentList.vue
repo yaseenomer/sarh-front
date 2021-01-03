d84edf4ff4028ea259e47348780643c41bfd7f41
yaseenomer <yaseeno373@gmail.com>
2021-01-03 17:29:14 +0300

    .333333

--- components/widget/post/postCommentList.vue
+++ components/widget/post/postCommentList.vue
@@ -1,19 +1,21 @@
 <template>
   <div>
     <v-list three-line>
-      <template v-for="(item, index) in comments">
-        <v-list-item :key="index">
+      <div v-for="(item, index) in comments" :key="index">
+        <v-list-item>
           <v-list-item-avatar>
             <v-avatar color="blue lighten-4">u</v-avatar>
           </v-list-item-avatar>
-
           <v-list-item-content>
-            <v-list-item-title v-text="item.user.name"></v-list-item-title>
+            <v-list-item-title>
+              <span>{{ item.user.name }}</span>
+              <span class="mx-2 primary--text">{{ item.created_at }}</span>
+            </v-list-item-title>
             <v-list-item-subtitle v-text="item.comment"></v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
-        <v-divider :key="index" inset />
-      </template>
+        <v-divider inset />
+      </div>
     </v-list>
   </div>
 </template>
